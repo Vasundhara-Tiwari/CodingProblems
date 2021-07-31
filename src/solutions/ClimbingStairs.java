bbf290f01b011c514ea3ef6bb22d35f2239c2b8e
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-01 01:15:24 +0530

    ClimbingStairs.java

---
+++ src/solutions/ClimbingStairs.java
@@ -0,0 +1,20 @@
+package solutions;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        printPossibleSteps(0,5,"");
+    }
+    public static void printPossibleSteps( int currentStep, int finalStep,String ans){
+        if(currentStep>finalStep){      //-ve base case
+            return;
+        }
+
+        if(currentStep==finalStep){     //+ve base case
+            System.out.println(ans);
+            return;
+        }
+        printPossibleSteps(currentStep+1,finalStep,ans+1);
+        printPossibleSteps(currentStep+2,finalStep, ans+2);
+        printPossibleSteps(currentStep+3,finalStep,ans+3);
+    }
+}
