44245b3715a6ef31ad2a9538b9a13f17f1d75998
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-07-31 23:54:13 +0530

    Adding a method to generate valid pair of parenthesis

--- src/solutions/GenerateValidParenthesisPair.java
+++ src/solutions/GenerateValidParenthesisPair.java
@@ -1,4 +1,15 @@
 package solutions;
 
 public class GenerateValidParenthesisPair {
+    public static void generateParenthesis(int n, int opening, int closing, String ans){
+        if(opening == n && closing == n){
+            System.out.println(ans);
+            return;
+        }
+        if(opening > n || closing > n || closing > opening){
+            return;
+        }
+        generateParenthesis(n, opening+1, closing, ans+"(");
+        generateParenthesis(n, opening, closing+1, ans+")");
+    }
 }
