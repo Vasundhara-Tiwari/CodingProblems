0f391f5a5846c0284ea87dd76ba4d85c0b7d41b9
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-01 15:34:14 +0530

    Sorting with most significant digits

--- src/solutions/JumpingNumbers.java
+++ src/solutions/JumpingNumbers.java
@@ -1,4 +1,24 @@
 package solutions;
 
+//jumping numbers have the consecutive digits difference = 1;
+//sorting by most significant digit
 public class JumpingNumbers {
+    public static void main(String[] args) {
+        for(int i=0; i<=9;i++) {
+            printJumpingNumbers(1000, i);
+        }
+    }
+    public static void printJumpingNumbers(int n, int current){
+        if(current > n){
+            return;
+        }
+        System.out.println(current);
+        int digit=current%10;
+        if(digit!=0) {
+            printJumpingNumbers(n, current * 10 + digit - 1);
+        }
+        if(digit!=9) {
+            printJumpingNumbers(n, current * 10 + digit + 1);
+        }
+    }
 }
