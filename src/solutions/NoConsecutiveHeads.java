081252228b33949651e008fcf7ed900df3df156c
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-01 14:36:04 +0530

    NoConsecutiveHeads on coin tosses

--- src/solutions/NoConsecutiveHeads.java
+++ src/solutions/NoConsecutiveHeads.java
@@ -1,4 +1,18 @@
 package solutions;
 
 public class NoConsecutiveHeads {
+    static boolean check =false;
+    public static void main(String[] args) {
+    noConsecutiveHeads(3,"",check);
+    }
+    public static void noConsecutiveHeads(int n, String ans, boolean check){
+        if(n==0){
+            System.out.println(ans);
+            return;
+        }
+        if(!check) {
+            noConsecutiveHeads(n - 1, ans + "H", true);
+        }
+        noConsecutiveHeads(n-1,ans+"T",false);
+    }
 }
