30ea656bf0ee8e3cd656421cfc2750be8b6617f4
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-01 11:36:18 +0530

    MazePath problem solution

--- src/solutions/MazePath.java
+++ src/solutions/MazePath.java
@@ -1,4 +1,20 @@
 package solutions;
 
 public class MazePath  {
+    public static void main(String[] args) {
+        mazePath(0,0,2,2,"");
+    }
+    public static void mazePath(int currentx, int currenty, int destinationx, int destinationy, String ans){
+        if(currentx>destinationx || currenty>destinationy){
+            return;
+        }
+
+        if(currentx==destinationx && currenty==destinationy){
+            System.out.println(ans);
+            return;
+        }
+
+        mazePath(currentx+1, currenty, destinationx, destinationy, ans+"D");
+        mazePath(currentx, currenty+1, destinationx, destinationy, ans+"R");
+    }
 }
