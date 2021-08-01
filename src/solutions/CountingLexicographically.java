a14bb826c7eb9d793d73bc4f0b2f29b647cf0a1c
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-01 15:00:35 +0530

    print numbers lexicographically

--- src/solutions/CountingLexicographically.java
+++ src/solutions/CountingLexicographically.java
@@ -1,4 +1,21 @@
 package solutions;
 
 public class CountingLexicographically {
+    public static void main(String[] args) {
+        printCountingLexicographically(1000, 0);
+    }
+    public static void printCountingLexicographically(int n, int current){
+        if(current>n){
+            return;
+        }
+
+        System.out.println(current);
+        int i=0;
+        if(current==0){
+            i=1;
+        }
+        for(;i<=9; i++){
+            printCountingLexicographically(n,current*10+i);
+        }
+    }
 }
