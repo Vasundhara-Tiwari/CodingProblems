9856314b95a838a83f0a6e2baac8cfd02a56184f
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-01 11:57:02 +0530

    Possible permutations of string

--- src/solutions/Permutations.java
+++ src/solutions/Permutations.java
@@ -1,4 +1,22 @@
 package solutions;
 
 public class Permutations {
+    public static void main(String[] args) {
+        possiblePermutations("abcd", "");
+    }
+    public static void possiblePermutations(String str, String ans){
+
+
+        if(str.isEmpty()){
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            String sp = str.substring(0,i) + str.substring(i+1);
+            possiblePermutations(sp, ans+ch);
+        }
+
+    }
 }
