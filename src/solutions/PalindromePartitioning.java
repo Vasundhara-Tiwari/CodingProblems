52ac00abca37f265f7c454d3aee556593ee96703
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-01 20:49:44 +0530

    Palindromic Partitions of a string

--- src/solutions/PalindromePartitioning.java
+++ src/solutions/PalindromePartitioning.java
@@ -1,4 +1,30 @@
 package solutions;
 
 public class PalindromePartitioning {
+    public static void main(String[] args) {
+        palindromePartition("nitin", "");
+    }
+    public static void palindromePartition(String str, String ans){
+        if(str.isEmpty()){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=1; i<=str.length(); i++) {
+            String chopped = str.substring(0, i);
+            String remaining = str.substring(i);
+            if (isPalindrome(chopped)) {
+                palindromePartition(remaining, ans + "," + chopped);
+            }
+        }
+    }
+    public static boolean isPalindrome(String str){
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
