c2c1749f73c515d739e85f687a82172fec6611bc
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-02 15:28:16 +0530

    Coin Denomination problem solution

--- src/solutions/CoinDenomination.java
+++ src/solutions/CoinDenomination.java
@@ -1,4 +1,21 @@
 package solutions;
 
 public class CoinDenomination {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,6};
+        coinDenominationAmount(arr, 10, "");
+    }
+    public static void coinDenominationAmount(int[] coins, int amount, String ans){
+
+        if(amount<0){
+            return;
+        }
+        if(amount==0){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0; i<coins.length; i++){
+            coinDenominationAmount(coins, amount-coins[i], ans+coins[i]);
+        }
+    }
 }
