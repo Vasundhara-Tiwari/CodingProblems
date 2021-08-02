16874f8148e8393448942143885923ccfd5a8c0f
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-02 19:30:11 +0530

    N queen permutations over 2d array

--- src/solutions/NQueen2D.java
+++ src/solutions/NQueen2D.java
@@ -1,4 +1,22 @@
 package solutions;
 
 public class NQueen2D {
+    public static void main(String[] args) {
+        nQueenPermutation(new boolean[2][2], 0,0,0,2,"");
+    }
+    public static void nQueenPermutation(boolean[][] board, int row,int col, int placedQueen, int totalQueen, String ans){
+        if(totalQueen==placedQueen){
+            System.out.println(ans);
+            return;
+        }
+        if(col==board[row].length){
+            row=row+1;
+            col=0;
+        }
+        if(row == board.length){
+            return;
+        }
+        nQueenPermutation(board, row,col+1,placedQueen+1, totalQueen, ans+"{"+row+","+col+"}");
+        nQueenPermutation(board,row,col+1, placedQueen,totalQueen,ans);
+    }
 }
