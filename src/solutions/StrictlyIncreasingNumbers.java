1734001fe0237bba703864bc2f2522dcdb768d7e
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-04 16:17:39 +0530

    solution to print strictly increasing numbers

--- src/solutions/StrictlyIncreasingNumbers.java
+++ src/solutions/StrictlyIncreasingNumbers.java
@@ -1,4 +1,17 @@
 package solutions;
 
 public class StrictlyIncreasingNumbers {
+    public static void main(String[] args) {
+        printStrictlyIncreasing("", -1, 3);
+    }
+    public static void printStrictlyIncreasing(String curr, int last_digit, int n) {
+        if(n==0) {
+            System.out.println(curr);
+            return;
+        }
+
+        for(int i=last_digit+1;i<=9;i++) {
+            printStrictlyIncreasing(curr+i, i, n-1);
+        }
+    }
 }
