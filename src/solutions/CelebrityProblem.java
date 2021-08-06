5dc20ca287692d1fc81dcb5ab8f4d8bd35e2b37b
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-06 21:22:31 +0530

    CelebrityProblem solution in java

---
+++ src/solutions/CelebrityProblem.java
@@ -0,0 +1,14 @@
+package solutions;
+
+public class CelebrityProblem {
+    public static void main(String[] args) {
+        int[][] arr = new int[4][4];
+        celebrity(arr);
+    }
+    public static int celebrity(int[][] arr){
+        for(int i=0; i<arr.length;i++){
+
+        }
+        return 0;
+    }
+}
