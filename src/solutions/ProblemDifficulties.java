1a6c03f4939cba86033d3268a734c1c3e08f3b23
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-07 18:48:56 +0530

    Problem Difficulties solution codechef

---
+++ src/solutions/ProblemDifficulties.java
@@ -0,0 +1,47 @@
+package solutions;
+
+import java.util.*;
+
+public class ProblemDifficulties {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner scn = new Scanner(System.in);
+        int test = scn.nextInt();
+        while (test-->0){
+            int[] arr = new int[4];
+            for(int i=0; i<arr.length; i++){
+                arr[i] = scn.nextInt();
+            }
+            ArrayList<Integer> al = new ArrayList<>();
+            for(int i=0; i<arr.length; i++){
+                if(!al.contains(arr[i])){
+                    al.add(arr[i]);
+                }
+            }
+            System.out.println(al);
+            if(al.size() > 2){
+                System.out.println(2);
+            } else if(al.size() == 2){
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+
+
+        /*for(int i=1; i<=test;i++) {
+            Set<Integer> al = new HashSet<>();
+            for (int j = 0; j < 4; j++) {
+                al.add(scn.nextInt());
+            }
+            if(al.size() > 2){
+                System.out.println("2");
+            } else if(al.size() == 2){
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        }*/
+    }
+}
