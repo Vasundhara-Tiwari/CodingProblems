da3c9279d7cc3d5d6362a6e113570b36d5047ac4
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-20 21:00:02 +0530

    implementing subsets method

--- src/solutions/Subsets.java
+++ src/solutions/Subsets.java
@@ -1,6 +1,9 @@
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
-        
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>(nums.length);
+        explore(0, nums, list, res);
+        return res;
     }
     
    
