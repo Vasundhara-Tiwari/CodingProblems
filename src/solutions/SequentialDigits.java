39178042211ccc1b0f445b4368ac9a0982188c80
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-01 23:10:53 +0530

    implementing method sequentialDigits

--- src/solutions/SequentialDigits.java
+++ src/solutions/SequentialDigits.java
@@ -1,7 +1,11 @@
 class SequentialDigits {
    public List<Integer> sequentialDigits(int low, int high) {
-        
-        
+        List<Integer> result = new ArrayList<>();
+        for(int i=1; i<=9; i++){
+            findSequentialDigits(i,low,high,result);
+        }
+        Collections.sort(result);
+        return result;
    }
    public static void findSequentialDigits(int current, int low, int high, List<Integer> result){
       
