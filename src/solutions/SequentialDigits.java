dce01b5d9790479862f6f0777885c1e167111289
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-01 23:12:21 +0530

    implementing findSequentialDigits method

--- src/solutions/SequentialDigits.java
+++ src/solutions/SequentialDigits.java
@@ -5,9 +5,16 @@ class SequentialDigits {
             findSequentialDigits(i,low,high,result);
         }
         Collections.sort(result);
-        return result;
+        
    }
    public static void findSequentialDigits(int current, int low, int high, List<Integer> result){
-      
+      if(current>=low && current<=high){
+         result.add(current);
+      }
+      int digit = current%10;
+      if(current>high || digit == 9){
+         return;
+      }
+      findSequentialDigits(current*10+digit+1,low,high,result);  
     }
 }
