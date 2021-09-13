4ecf18bab4f7167b43ad66577ca26dbdea3e47a8
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-13 16:36:24 +0530

    Added the solution

--- src/solutions/MaximumNumberOfBalloons.java
+++ src/solutions/MaximumNumberOfBalloons.java
@@ -5,6 +5,20 @@ public class MaximumNumberOfBalloons {
 
     }
     public int maxNumberOfBalloons(String text) {
+        char[] arr = {'b', 'a', 'l','o', 'n'};
+        int[] frequency = new int[arr.length];
+        for(int j=0; j<arr.length; j++){
+            int count = 0;
+            for(int i=0; i<text.length(); i++){
+                if(text.charAt(i) == arr[j]){
+                    count++;
+                }
+            }
+            frequency[j]=count;
+        }
+        frequency[2] /= 2;
+        frequency[3] /= 2;
 
+        return Math.min(frequency[0], Math.min(frequency[1], Math.min(frequency[2], Math.min(frequency[3], frequency[4]))));
     }
 }
