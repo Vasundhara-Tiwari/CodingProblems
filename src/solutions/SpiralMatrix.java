55ed895ed836d1fbdce6396b6b1eafeb5e0836da
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-16 19:37:13 +0530

    https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/638/week-3-september-15th-september-21st/3977/ problem solution

--- src/solutions/SpiralMatrix.java
+++ src/solutions/SpiralMatrix.java
@@ -9,6 +9,42 @@ public class SpiralMatrix {
         System.out.println(spiralOrder(arr));
     }
     public static List<Integer> spiralOrder(int[][] matrix) {
-
+        List<Integer> al = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return al;
+        }
+        int min_row = 0, max_row = matrix.length - 1;
+        int min_col = 0, max_col = matrix[0].length - 1;
+        while (true) {
+            if (min_col > max_col) {
+                break;
+            }
+            for (int i = min_col; i <= max_col; i++) {
+                al.add(matrix[min_row][i] );
+            }
+            min_row++;
+            if (min_row > max_row) {
+                break;
+            }
+            for (int i = min_row; i <= max_row; i++) {
+                al.add(matrix[i][max_col]);
+            }
+            max_col--;
+            if (min_col > max_col) {
+                break;
+            }
+            for (int i = max_col; i >= min_col; i--) {
+                al.add(matrix[max_row][i]);
+            }
+            max_row--;
+            if (min_row > max_row) {
+                break;
+            }
+            for (int i = max_row; i >= min_row; i--) {
+                al.add(matrix[i][min_col]);
+            }
+            min_col++;
+        }
+        return al;
     }
 }
