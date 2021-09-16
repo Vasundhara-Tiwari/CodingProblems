6e249dfeb621828512053a44cb33a4ec0812e711
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-16 19:36:35 +0530

    adding method for the problem solution https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/638/week-3-september-15th-september-21st/3977/

--- src/solutions/SpiralMatrix.java
+++ src/solutions/SpiralMatrix.java
@@ -1,13 +1,14 @@
 package solutions;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpiralMatrix {
     public static void main(String[] args) {
-        int[][] arr = {{1,2,3}, {4,5,6},{7,8,9}, {10,11,12}};
-        for(int i=0; i< arr.length; i++) {
-            System.out.println(Arrays.toString(arr[i]));
-        }
+        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         System.out.println(spiralOrder(arr));
+    }
+    public static List<Integer> spiralOrder(int[][] matrix) {
+
     }
 }
