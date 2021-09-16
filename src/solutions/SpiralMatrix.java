f1ddb1ddd64822e673ec9042a99d47693ff5e9c0
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-16 19:29:46 +0530

    Adding main Method

--- src/solutions/SpiralMatrix.java
+++ src/solutions/SpiralMatrix.java
@@ -1,4 +1,13 @@
 package solutions;
 
+import java.util.Arrays;
+
 public class SpiralMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3}, {4,5,6},{7,8,9}, {10,11,12}};
+        for(int i=0; i< arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+        System.out.println(spiralOrder(arr));
+    }
 }
