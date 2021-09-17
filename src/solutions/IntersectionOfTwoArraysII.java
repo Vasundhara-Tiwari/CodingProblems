b6e9c65eded3647cfd56f25f16901a8613312b5e
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-17 22:18:41 +0530

    debugging

--- src/solutions/IntersectionOfTwoArraysII.java
+++ src/solutions/IntersectionOfTwoArraysII.java
@@ -1,12 +1,15 @@
 package solutions;
 
+import java.util.Arrays;
+
 public class IntersectionOfTwoArraysII {
     public static void main(String[] args) {
 
     }
-    public static int[] intersect(int nums1, int nums2){
+    public static int[] intersect(int[] nums1, int[] nums2){
 
-        int[] result = new int[];
+        int[] result = new int[k];
+
         return result;
     }
 }
