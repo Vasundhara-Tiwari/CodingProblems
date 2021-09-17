b2dcd7378f852e4de28614b64cb8cf3e50fd2af5
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-17 22:19:01 +0530

    added solution of https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/638/week-3-september-15th-september-21st/3978/

--- src/solutions/IntersectionOfTwoArraysII.java
+++ src/solutions/IntersectionOfTwoArraysII.java
@@ -7,9 +7,30 @@ public class IntersectionOfTwoArraysII {
 
     }
     public static int[] intersect(int[] nums1, int[] nums2){
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0,j=0,k=0;
+        while(i<nums1.length && j<nums2.length)
+        {
+            if(nums1[i]<nums2[j])
+            {
+                i++;
+            }
+            else if(nums1[i]>nums2[j])
+            {
+                j++;
+            }
+            else
+            {
+                nums1[k++]=nums1[i];
+                ++i;++j;
+            }
+        }
 
         int[] result = new int[k];
-
+        for(i=0;i<k;i++){
+            result[i]=nums1[i];
+        }
         return result;
     }
 }
