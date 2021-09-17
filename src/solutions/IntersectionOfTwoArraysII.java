a526edbc3d59a9400bc97bf62e6006bde322150b
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-17 22:16:31 +0530

    Added method for solution logic

--- src/solutions/IntersectionOfTwoArraysII.java
+++ src/solutions/IntersectionOfTwoArraysII.java
@@ -4,4 +4,9 @@ public class IntersectionOfTwoArraysII {
     public static void main(String[] args) {
 
     }
+    public static int[] intersect(int nums1, int nums2){
+
+        int[] result = new int[];
+        return result;
+    }
 }
