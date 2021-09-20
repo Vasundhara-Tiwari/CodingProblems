a478bbc496a6a6be033224cb9f65c3418da140f4
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-20 23:24:55 +0530

    Solution of https://leetcode.com/problems/container-with-most-water/

--- src/solutions/ContainerWithMostWater.java
+++ src/solutions/ContainerWithMostWater.java
@@ -1,11 +1,48 @@
 package solutions;
 
+import java.util.Scanner;
+
 public class ContainerWithMostWater {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] height = new int[size];
+        for(int i=0; i<height.length; i++){
+            height[i]=scanner.nextInt();
+        }
+        System.out.println(maxArea(height));
     }
-    public int maxArea(int[] height) {
+    public static int maxArea(int[] height) {
 
+        int start =0, end = height.length -1;
+        int result =0, area=0;
 
+        while(start <end) {
+            area = (end- start)*Math.min(height[start],height[end]);
+            if(area> result)
+                result =area;
+
+            if(height[start]<height[end])
+                start++;
+
+            else if(height[start]>height[end])
+                end--;
+
+            else {
+                if(end- start ==1)
+                    break;
+
+                if(end- start ==2)
+                    start++;
+
+                else if(height[end-1]>height[start +1])
+                    end--;
+
+                else
+                    start++;
+            }
+        }
+
+        return result;
     }
 }
