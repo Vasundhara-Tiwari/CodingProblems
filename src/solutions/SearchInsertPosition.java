7d1f797892331a86cec06c985f65778855f3c615
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-25 18:16:03 +0530

    called function for the output

--- src/solutions/SearchInsertPosition.java
+++ src/solutions/SearchInsertPosition.java
@@ -7,6 +7,11 @@ public class SearchInsertPosition {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
         int[] arr = new int[size];
+        for(int i=0; i<arr.length;i++){
+            arr[i] = scanner.nextInt();
+        }
+        int target = scanner.nextInt();
+        System.out.println(searchInsert(arr,target));
     }
     public static int searchInsert(int[] nums, int target) {
         return 0;
