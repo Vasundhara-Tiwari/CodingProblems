a84f64e91372fa8eb21e4b842d7f8e9a811992c2
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-09-25 18:14:33 +0530

    implemented main

--- src/solutions/SearchInsertPosition.java
+++ src/solutions/SearchInsertPosition.java
@@ -1,8 +1,12 @@
 package solutions;
 
+import java.util.Scanner;
+
 public class SearchInsertPosition {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
     }
     public static int searchInsert(int[] nums, int target) {
         return 0;
