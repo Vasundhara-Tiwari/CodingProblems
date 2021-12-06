a658baa441c52888cb27ec57dab150c79552f5ae
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-06 21:34:14 +0530

    implementing problem logic

--- src/solutions/MoveChips.java
+++ src/solutions/MoveChips.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class MoveChips {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[3];
+        for(int i=0; i< 3; i++){
+            arr[i] = scanner.nextInt();
+        }
+    }
+    public int minCostToMoveChips(int[] position) {
+        int sum1 = 0;
+        int sum2 = 0;
+
+        return (sum1 < sum2) ? sum1 : sum2;
     }
 }
