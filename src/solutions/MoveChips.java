2197b0f7cfc7e5ca3ca2094f2c340c5d32633cc8
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-06 21:31:56 +0530

    importing and creating object of Scanner class

--- src/solutions/MoveChips.java
+++ src/solutions/MoveChips.java
@@ -1,7 +1,9 @@
 package solutions;
 
+import java.util.Scanner;
+
 public class MoveChips {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
