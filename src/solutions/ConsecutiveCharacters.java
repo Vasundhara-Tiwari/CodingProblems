edda7462f08b51956611a87b668e4f78674b43b7
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-13 22:47:34 +0530

    adding logic for problem solution

--- src/solutions/ConsecutiveCharacters.java
+++ src/solutions/ConsecutiveCharacters.java
@@ -3,6 +3,9 @@ package solutions;
 public class ConsecutiveCharacters {
     public int maxPower(String s){
         int max = 1, count = 1;
+        for(int i=0;i<s.length()-1;i++) {
+
+        }
         return max;
     }
 }
