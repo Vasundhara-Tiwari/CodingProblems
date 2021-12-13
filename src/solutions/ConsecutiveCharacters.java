2dd1d2a8cd35b53973a3cd7faa19b79c90819417
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-13 22:48:45 +0530

    creating main method

--- src/solutions/ConsecutiveCharacters.java
+++ src/solutions/ConsecutiveCharacters.java
@@ -1,6 +1,9 @@
 package solutions;
 
 public class ConsecutiveCharacters {
+    public static void main(String[] args) {
+
+    }
     public int maxPower(String s){
         int max = 1, count = 1;
         for(int i=0;i<s.length()-1;i++) {
