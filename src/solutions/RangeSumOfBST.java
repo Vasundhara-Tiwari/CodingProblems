eabd9cad03b7444b3dcab3e26155c651f9d4f916
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-14 18:46:26 +0530

    Adding TreeNode Definition for solution of the problem

--- src/solutions/RangeSumOfBST.java
+++ src/solutions/RangeSumOfBST.java
@@ -1,4 +1,20 @@
 package solutions;
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 public class RangeSumOfBST {
 }
