546d4a5fb9e87f50156679bfe4a49cee636ca43a
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-16 20:03:35 +0530

    adding solution of https://leetcode.com/problems/length-of-last-word/

--- src/solutions/LengthOfLastWord.java
+++ src/solutions/LengthOfLastWord.java
@@ -4,7 +4,9 @@ public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
         int len=0;
         s=s.strip();
+        for(int i=s.length()-1;i>=0;i--){
 
+        }
         return len;
     }
 }
