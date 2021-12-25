6ea14dc5b61ed8542ee3a7bbab6b1dbe0f240924
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-25 21:22:38 +0530

    adding function call to subsets from main

--- src/solutions/Subsets.java
+++ src/solutions/Subsets.java
@@ -5,13 +5,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Subsets {
-    public List<List<Integer>> subsets(int[] nums) {
+    public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> list = new ArrayList<>(nums.length);
         explore(0, nums, list, res);
         return res;
     }
-    public void explore(int start, int[] nums, List<Integer> list, List<List<Integer>> res) {
+    public static void explore(int start, int[] nums, List<Integer> list, List<List<Integer>> res) {
         res.add(new ArrayList<>(list));
         for (int i = start; i < nums.length; i++) {
             list.add(nums[i]);
@@ -19,7 +19,6 @@ public class Subsets {
             list.remove(list.size() - 1);
         }
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
@@ -27,5 +26,6 @@ public class Subsets {
         for(int i=0; i<size; i++){
             arr[i] = scanner.nextInt();
         }
+        subsets(arr);
     }
 }
