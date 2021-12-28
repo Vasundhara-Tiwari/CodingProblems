206c590a5eaa00b1515994cc3cb009b0ae2bb529
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-12-28 21:41:50 +0530

    https://leetcode.com/problems/middle-of-the-linked-list/submissions/ solution

--- src/solutions/MiddleOfLinkedList.java
+++ src/solutions/MiddleOfLinkedList.java
@@ -1,5 +1,13 @@
 package solutions;
 
 public class MiddleOfLinkedList {
-
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode middleNode(ListNode head) {
+    }
 }
