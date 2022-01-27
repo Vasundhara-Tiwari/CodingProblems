49ba24c19b29a6b56c5ade0d30e1859ad47c3c73
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-01-27 23:16:10 +0530

    solution of https://leetcode.com/problems/house-robber/

--- src/solutions/HouseRobber.java
+++ src/solutions/HouseRobber.java
@@ -1,5 +1,13 @@
 package solutions;
 
+import java.util.Arrays;
+
 public class HouseRobber {
     static int index = 0;
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length+1];
+        Arrays.fill(dp, -1);
+
+
+    }
 }
