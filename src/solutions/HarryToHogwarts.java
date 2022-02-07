7974a61d4187d99f15ee80caf3b7b460efb5fc4c
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-02-07 23:31:58 +0530

    HarryToHogwarts.java

--- src/solutions/HarryToHogwarts.java
+++ src/solutions/HarryToHogwarts.java
@@ -4,6 +4,53 @@ import java.util.Scanner;
 
 public class HarryToHogwarts {
 
+    public static int smallestNumber(int number) {
+        int[] frequency = new int[10];
+        boolean isPosition = (number >0);
+        number = Math.abs(number);
+        while (number > 0) {
+            int d = number % 10;
+            if(d==0){
+                frequency[0]++;
+            }
+            frequency[d]++;
+            number = number / 10;
+        }
+        int result = 0;
+        while (frequency[0]>0){
+            result='0'+result;
+            frequency[0]= frequency[0]-1;
+        }
+        if(isPosition) {
+            for (int i = 0 ; i <= 9 ; i++) {
+                if (frequency[i] !=0) {
+                    result = i;
+                    frequency[i]--;
+                    break;
+                }
+            }
+            for (int i = 0 ; i <= 9 ; i++)
+                while (frequency[i]-- != 0)
+                    result = result * 10 + i;
+
+        }
+        else {
+            for (int i = 9 ; i >= 0 ; i--) {
+                if (frequency[i] !=0) {
+                    result = i;
+                    frequency[i]--;
+                    break;
+                }
+            }
+            for (int i =9 ; i >=0 ; i--)
+                while (frequency[i]-- != 0)
+                    result = result * 10 + i;
+
+            result = -result;
+        }
+        return result;
+    }
+
     static int printMaximum(int number) {
         int[] count = new int[10];
         String str = Integer.toString(number);
