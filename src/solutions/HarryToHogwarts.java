20def7232dd9aa44a93be38ca99a253336d430e2
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-02-07 23:31:37 +0530

    HarryToHogwarts.java

--- src/solutions/HarryToHogwarts.java
+++ src/solutions/HarryToHogwarts.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class HarryToHogwarts {
 
+    static int printMaximum(int number) {
+        int[] count = new int[10];
+        String str = Integer.toString(number);
+        for (int i = 0; i < str.length(); i++)
+            count[str.charAt(i) - '0']++;
+        int result = 0, multiplier = 1;
+        for (int i = 0; i <= 9; i++) {
+            while (count[i] > 0) {
+                result = result + (i * multiplier);
+                count[i]--;
+                multiplier = multiplier * 10;
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         countNumberOfTimes(sc.nextInt());
