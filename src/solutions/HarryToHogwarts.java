480ad4e817d159406a3986287daba644f9ab5477
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-02-07 23:31:20 +0530

    HarryToHogwarts.java

---
+++ src/solutions/HarryToHogwarts.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class HarryToHogwarts {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        countNumberOfTimes(sc.nextInt());
+        System.out.println(count);
+    }
+}
