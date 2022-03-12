1aaeb43427d7befba2b6f83e2e5b6372e9ce734b
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-03-13 00:03:35 +0530

    RemoveNthNodeFromLast

--- src/solutions/RemoveNthNodeFromLast.java
+++ src/solutions/RemoveNthNodeFromLast.java
@@ -6,5 +6,31 @@ package solutions;
  */
 
 public class RemoveNthNodeFromLast {
-
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+   }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode current = head;
+        int size = 0;
+        while(current != null){
+            current = current.next;
+            size++;
+        }
+        if(n == size){
+            return head.next;
+        }
+        int nodeRemove = size - n;
+        current = head;
+        int i=1;
+        while(i < nodeRemove){
+            current = current.next;
+            i++;
+        }
+        current.next = current.next.next;
+        return head;
+    }
 }
