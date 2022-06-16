75bac8bae3dca15a134c307afab1a7a05b5e56f3
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-16 23:25:12 +0530

    adding solution of https://leetcode.com/problems/valid-parentheses/

--- src/solutions/ValidPairParenthesis.java
+++ src/solutions/ValidPairParenthesis.java
@@ -1,1 +1,32 @@
-
+public class ValidPairParenthesis {
+  public boolean isValid(String input_str) {
+        Stack<Character> s = new Stack<Character>();
+        for (char st : input_str.toCharArray()) {
+            if (st == '(' || st == '{' || st == '[') {
+                s.push(st);
+            }
+            else {
+                if(s.empty()) {
+                    return false;
+                }
+                else{
+                    char top = (Character) s.peek();
+                    if(st == ')' && top == '(' ||
+                       st == '}' && top == '{' ||
+                       st == ']' && top == '['){
+                        s.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if(s.empty()) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
