f4a51ac8b70e5c7bdf9705380189b2b30a71c7ca
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-17 23:41:03 +0530

    solution of https://leetcode.com/problems/remove-element/

--- src/solutions/RemoveElement.java
+++ src/solutions/RemoveElement.java
@@ -1,2 +1,11 @@
 public class RemoveElement{
+  public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
 }
