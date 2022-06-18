511bfbcac01edc783625049d8b3fe0f3e115837d
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-18 23:35:08 +0530

    adding solution of https://leetcode.com/problems/merge-sorted-array/

--- src/solutions/Merge.java
+++ src/solutions/Merge.java
@@ -1,5 +1,32 @@
 public class Merge{
   public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
+        int []arr = new int[m+n];
+        int i = 0;
+        int j=0;
+        int k = 0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                arr[k] = nums1[i];
+                i++;
+                k++;
+            }else{
+                arr[k] = nums2[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<m){
+            arr[k] = nums1[i];
+            k++;
+            i++;
+        }
+        while(j<n){
+            arr[k] = nums2[j];
+            k++;
+            j++;
+        }
+        for(int l = 0; l < arr.length; l++){
+            nums1[l] = arr[l];
+        }
     }
 }
