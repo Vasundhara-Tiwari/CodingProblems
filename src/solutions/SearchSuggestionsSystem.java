6188cc9ee890a4c5d251f8058246d0c46cd437af
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-19 23:37:08 +0530

    adding solution of https://leetcode.com/problems/search-suggestions-system/

--- src/solutions/SearchSuggestionsSystem.java
+++ src/solutions/SearchSuggestionsSystem.java
@@ -1,6 +1,10 @@
 public class SearchSuggestionsSystem{
   public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        List<List<String>> al = new ArrayList<>();
-        Arrays.sort(products);
+    List<List<String>> al = new ArrayList<>();
+    Arrays.sort(products);
+    for(int i = 0; i < searchWord.length(); i++){
+            
     }
+    return al;
+  }
 }
