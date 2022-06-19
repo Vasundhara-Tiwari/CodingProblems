db081ec522e47298d7591663fc121ad57c8a3236
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-19 23:38:32 +0530

    adding solution of https://leetcode.com/problems/search-suggestions-system/

--- src/solutions/SearchSuggestionsSystem.java
+++ src/solutions/SearchSuggestionsSystem.java
@@ -3,7 +3,17 @@ public class SearchSuggestionsSystem{
     List<List<String>> al = new ArrayList<>();
     Arrays.sort(products);
     for(int i = 0; i < searchWord.length(); i++){
-            
+      List<String> nested = new ArrayList<>();
+      String str = searchWord.substring(0, i+1);
+      for(int j = 0; j < products.length; j++){
+        if(nested.size() >= 3){
+          break;
+        }
+        if(products[j].startsWith(str)){
+          nested.add(products[j]);
+        }
+      }
+      al.add(nested);
     }
     return al;
   }
