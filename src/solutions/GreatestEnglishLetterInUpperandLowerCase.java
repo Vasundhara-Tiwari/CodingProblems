350259e4fbdd379a3fb6f807fbcfff5125107c95
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-20 23:20:37 +0530

    adding solution of https://leetcode.com/contest/weekly-contest-298/problems/greatest-english-letter-in-upper-and-lower-case/

--- src/solutions/GreatestEnglishLetterInUpperandLowerCase.java
+++ src/solutions/GreatestEnglishLetterInUpperandLowerCase.java
@@ -1,5 +1,7 @@
 public class GreatestEnglishLetterInUpperandLowerCase{
   public String greatestLetter(String s) {
-  
+    ArrayList<Character> uppercase = new ArrayList<>();
+    ArrayList<Character> lowercase = new ArrayList<>();
+    ArrayList<Character> common = new ArrayList<>();    
   }
 }
