ee5c0b61774d5334b3a66be43b962abc76e5d64a
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-21 23:26:43 +0530

    adding main method

--- src/solutions/GreatestNumberOfRepeatedLetters.java
+++ src/solutions/GreatestNumberOfRepeatedLetters.java
@@ -1,3 +1,8 @@
 public class GreatestNumberOfRepeatedLetters{
-
+  public static void main (String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final String str = scanner.nextLine();
+        scanner.close();
+        System.out.print(searchingChallenge(str));
+    }
 }
