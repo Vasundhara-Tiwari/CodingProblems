a380bd62b3aea555dff9cdf934f80f10eb7b5d2d
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-21 23:28:48 +0530

    adding solution

--- src/solutions/GreatestNumberOfRepeatedLetters.java
+++ src/solutions/GreatestNumberOfRepeatedLetters.java
@@ -9,7 +9,24 @@ public class GreatestNumberOfRepeatedLetters{
         String challengeToken = "grdxcz850";
         int maxChar = 0;
         String result = "";
-
+        final String[] words = str.split("\\s+");
+        for (final String word : words) {
+            final int c = mostFrequentCharCount(word);
+            if (c > maxChar) {
+                maxChar = c;
+                result = word;
+            }
+        }
+        result += challengeToken;
+        String finaloutput = "";
+        for(int i = 0; i < result.length(); i++){
+            if(i%3 == 2){
+                finaloutput += "X";
+            }
+            else {
+                finaloutput += result.substring(i, i + 1);
+            }
+        }
         
         return finaloutput;
     }
