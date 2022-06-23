509164dd1d6d6d3d6b5032e2bd0c4142b7a12570
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-23 22:44:00 +0530

    adding solution of https://leetcode.com/problems/course-schedule-iii/submissions/

--- src/solutions/CourseScheduleIII.java
+++ src/solutions/CourseScheduleIII.java
@@ -1,5 +1,8 @@
 public class CourseScheduleIII{
   public int scheduleCourse(int[][] courses) {
+        var maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
+        Arrays.sort(courses, Comparator.comparingInt(course -> course[1]));
         
+        return maxHeap.size();
     }
 }
