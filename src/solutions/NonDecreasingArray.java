083492aefc7a1b730dfc3d4ab43b0fc7906408ee
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-25 22:54:37 +0530

    adding solution of https://leetcode.com/problems/non-decreasing-array/

--- src/solutions/NonDecreasingArray.java
+++ src/solutions/NonDecreasingArray.java
@@ -1,7 +1,11 @@
 public class NonDecreasingArray{
   public boolean checkPossibility(int[] nums) {
         int modified = 0;
-        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                
+            }
+        }
         return true;
     }
 }
