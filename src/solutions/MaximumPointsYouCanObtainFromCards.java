2298df2962980210953589b94117f6641b1a7f08
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-26 22:50:35 +0530

    adding solution of https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards

--- src/solutions/MaximumPointsYouCanObtainFromCards.java
+++ src/solutions/MaximumPointsYouCanObtainFromCards.java
@@ -1,6 +1,8 @@
 public class MaximumPointsYouCanObtainFromCards{
   public int maxScore(int[] cardPoints, int k) {
          int n = cardPoints.length, maximumScore = 0, leftSum = 0, rightSum = 0;
+         for(int i = 0; i < k; i++) 
+           leftSum += cardPoints[i];
          return maximumScore;
     }
 }
