580ec2f3903e6d841836e6f847833942829c428b
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-29 23:20:37 +0530

    adding solution of https://leetcode.com/problems/queue-reconstruction-by-height

--- src/solutions/QueueReconstructionByHeight.java
+++ src/solutions/QueueReconstructionByHeight.java
@@ -1,5 +1,8 @@
 public class QueueReconstructionByHeight{
   public int[][] reconstructQueue(int[][] people) {
     Arrays.sort(people, (a,b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+    List<int[]> ordered = new LinkedList<>();
+       
+	  return ordered.toArray(new int[people.length][2]);
   }
 }
