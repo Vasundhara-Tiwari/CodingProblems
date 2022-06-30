5f4cf939bbd695b88c04ca90f757e416f378f7e0
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-06-30 22:53:29 +0530

    adding solution of https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii

--- src/solutions/MinimumMovesToEqualArrayElements.java
+++ src/solutions/MinimumMovesToEqualArrayElements.java
@@ -1,5 +1,7 @@
 public class MinimumMovesToEqualArrayElements{
   public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int mid = nums[nums.length/2];
         int moves = 0;
         
         return moves;
