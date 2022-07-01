a2b25ee45ef20269d6723eaad4bd0ae5841ba7ed
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-01 23:16:21 +0530

    adding solution of https://leetcode.com/problems/maximum-units-on-a-truck

--- src/solutions/MaximumUnitsOnATruck.java
+++ src/solutions/MaximumUnitsOnATruck.java
@@ -1,5 +1,13 @@
 public class MaximumUnitsOnATruck{
   public int maximumUnits(int[][] boxTypes, int truckSize) {
-  
+    Arrays.sort(boxTypes, (x, y) -> y[1] - x[1]);
+    int maxUnits = 0;
+    for(int[] b : boxTypes) {
+      int count = Math.min(b[0], truckSize);
+      maxUnits += count * b[1];
+      truckSize -= count;
+      if(truckSize == 0) return maxUnits;
+    }
+    return maxUnits;
   }
 }
