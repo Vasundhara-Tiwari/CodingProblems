37a70b685ddd03584c778308d5ca2293aa4168f4
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-02 22:34:34 +0530

    adding solution of https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/

--- src/solutions/MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.java
+++ src/solutions/MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.java
@@ -2,5 +2,9 @@ public class MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts{
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
         Arrays.sort(horizontalCuts);
         Arrays.sort(verticalCuts);
+        int maxh = horizontalCuts[0];
+        for(int i=1;i<horizontalCuts.length;i++)
+            maxh = Math.max(horizontalCuts[i]-horizontalCuts[i-1],maxh);
+        maxh = Math.max(h-horizontalCuts[horizontalCuts.length-1],maxh);
     }
 }
