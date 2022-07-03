0bcee73169af541cdcf4a24b1bda24c6873cc338
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-03 23:21:54 +0530

    adding solution of https://leetcode.com/problems/wiggle-subsequence/

--- src/solutions/WiggleSubsequence.java
+++ src/solutions/WiggleSubsequence.java
@@ -5,6 +5,13 @@ public class WiggleSubsequence{
     }
     int count=1;
     int prevDiff=0;
+    for(int i=1;i<nums.length;i++){
+      int diff = nums[i] - nums[i-1];
+      if((diff>0 && prevDiff <= 0) || (diff<0 && prevDiff >= 0)){
+        count++;
+        prevDiff = diff;
+      }
+    }
     return count;
   }
 }
