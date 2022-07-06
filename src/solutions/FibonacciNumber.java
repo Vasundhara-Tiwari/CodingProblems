a78fd063871698ad6a97f636330d6a5b8a26b180
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-06 11:20:11 +0530

    adding solution of https://leetcode.com/problems/fibonacci-number/

--- src/solutions/FibonacciNumber.java
+++ src/solutions/FibonacciNumber.java
@@ -1,2 +1,13 @@
 public class FibonacciNumber{
+  public int fib(int n) {
+        if(n == 1){
+            return 1;
+        }
+        if(n == 0){
+            return 0;
+        }
+        int num1 = fib(n-1);
+        int num2 = fib(n-2);
+        return num1 + num2;
+    }
 }
