37af1c50548eb852d4fe9391419b1f31250a1364
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-10 23:01:16 +0530

    adding solution of https://leetcode.com/problems/min-cost-climbing-stairs/

--- src/solutions/MinCostClimbingStairs.java
+++ src/solutions/MinCostClimbingStairs.java
@@ -1,4 +1,7 @@
 public class MinCostClimbingStairs{
   public int minCostClimbingStairs(int[] cost) {
+    for (int i = 2; i < cost.length; i ++) {
+      cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+    }
   }
 }
