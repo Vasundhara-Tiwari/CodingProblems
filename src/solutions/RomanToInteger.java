50ee72d6996b4c85ada280248b379172351a6b7c
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-11 23:37:05 +0530

    adding solution of https://leetcode.com/problems/roman-to-integer/

--- src/solutions/RomanToInteger.java
+++ src/solutions/RomanToInteger.java
@@ -1,4 +1,37 @@
 public class RomanToInteger{
   public int romanToInt(String s) {
+    int result=0, i=0;
+    while(i < s.length()) {
+      if(i+1 < s.length()) {
+        if(s.charAt(i) == 'I') {
+          if(s.charAt(i+1) == 'V') {
+            result += 4;
+            i+=2;
+            continue;
+          } else if(s.charAt(i+1) == 'X') {
+            result += 9;
+            i+=2;
+            continue;
+          }
+        }
+        switch(s.charAt(i)) {
+          case 'I': result += 1;
+            break;
+          case 'V': result += 5;
+            break;
+          case 'X': result += 10;
+            break;
+          case 'L': result += 50;
+            break;
+          case 'C': result += 100;
+            break;
+          case 'D': result += 500;
+            break;
+          case 'M': result += 1000;
+            break;
+        }
+        i++;
+      } 
+      return result;
   }
 }
