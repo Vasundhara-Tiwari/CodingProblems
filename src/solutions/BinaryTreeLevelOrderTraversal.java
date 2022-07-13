ec4106e53f26cc66c77d02b793c4b7e26e645929
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-13 22:38:34 +0530

    adding solution of https://leetcode.com/problems/binary-tree-level-order-traversal

--- src/solutions/BinaryTreeLevelOrderTraversal.java
+++ src/solutions/BinaryTreeLevelOrderTraversal.java
@@ -1,2 +1,8 @@
 public class BinaryTreeLevelOrderTraversal{
+  
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> levelOrder = new ArrayList<>();
+        traverseTreeInLveleOrder(root, 1, levelOrder);
+        return levelOrder;
+    }
 }
