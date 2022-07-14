41776390a87da5d3142d93c0598ad00fb683ab83
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-14 22:51:46 +0530

    adding solution of https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/

--- src/solutions/BinaryTreeFromPreorderInorderTraversal.java
+++ src/solutions/BinaryTreeFromPreorderInorderTraversal.java
@@ -1,6 +1,21 @@
 public class BinaryTreeFromPreorderInorderTraversal{
   int index = 0;
   public TreeNode buildTree(int[] preorder, int[] inorder) {
-    return build(preorder, inorder, 0, preorder.length-1);
+        return build(preorder, inorder, 0, preorder.length-1);
   }
+  public TreeNode build(int[] preorder, int[] inorder, int left, int right){
+        if(index == preorder.length || left > right){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[index]);
+        for(int i = left; i<=right; i++){
+            if(preorder[index] == inorder[i]){
+                index++;
+                root.left = build(preorder, inorder, left, i-1);
+                root.right = build(preorder, inorder, i+1, right);
+                break;
+            }
+        }
+        return root;
+    }
 }
