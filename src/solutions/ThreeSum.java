6fa56dc4bb38ef0f07f008279fda1b6a7d064c7d
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-15 23:32:45 +0530

    adding solution of https://leetcode.com/problems/3sum/

--- src/solutions/ThreeSum.java
+++ src/solutions/ThreeSum.java
@@ -1,5 +1,8 @@
 public class ThreeSum{
   public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> triplets = new HashSet<>();
+        Arrays.sort(nums);
         
+        return new ArrayList<List<Integer>>(triplets);
     }
 } 
