2b79d23de2fa37539631072f78e30c79a16208d9
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-20 22:35:49 +0530

    added solution

--- src/solutions/CommonChild.java
+++ src/solutions/CommonChild.java
@@ -23,5 +23,13 @@ public class CommonChild {
                 count[j]=Math.min(count1[j],count[j]);
             }
         }
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
+                for(int j=0;j<count[i];j++){
+                    ls.add(""+(char)(i+'a'));
+                }
+            }
+        }
+        return ls.size();
     }
 }
