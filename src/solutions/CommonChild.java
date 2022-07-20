f8304ad77105ada61d59e999e3af8d1af6a79251
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-20 22:34:19 +0530

    creating main method

--- src/solutions/CommonChild.java
+++ src/solutions/CommonChild.java
@@ -1,1 +1,9 @@
-
+public class CommonChild {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        String[] words = {str1, str2};
+        System.out.println(commonChars(words));
+    }
+}
