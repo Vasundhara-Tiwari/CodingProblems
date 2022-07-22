556f713963fc3fb368d202c7d72162acd73d9b56
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-22 23:39:26 +0530

    adding solution

--- src/solutions/DataMigration.java
+++ src/solutions/DataMigration.java
@@ -1,1 +1,18 @@
+package solutions;
 
+import java.util.Arrays;
+
+/**
+ * Created by Vasundhara Tiwari
+ * On 20-06-2022 at 19:28
+ */
+
+public class DataMigration {
+    public static void main(String[] args) {
+        String[] sourceArray = {"1234", "2325", "122363", "32435" };
+        String[] destinationArray = {"1234", "2345", "122363", "32435" };
+        int[] output = new int[2];
+        int corruptedIndex = 0;
+       
+    }
+}
