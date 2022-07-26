582da7dd1ed802260a699071723fed025094ff4f
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-26 22:47:33 +0530

    adding base condition

--- src/solutions/LowestCommonAncestor.java
+++ src/solutions/LowestCommonAncestor.java
@@ -1,5 +1,7 @@
 public class LowestCommonAncestor{
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+        if(root == null || p == root || q == root){
+            return root;
+        }
   }
 }
