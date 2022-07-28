37cc4309eb3f829842b335936ef9d009ab316761
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-28 22:48:11 +0530

    adding solution of https://leetcode.com/problems/valid-anagram/

--- src/solutions/ValidAnagram.java
+++ src/solutions/ValidAnagram.java
@@ -1,7 +1,12 @@
 public class ValidAnagram{
   public boolean isAnagram(String s, String t) {
+    
     char[] arr1 = s.toCharArray();
     char[] arr2 = t.toCharArray();
+    
+    Arrays.sort(arr1);
+    Arrays.sort(arr2);
+    
     return false;
   }
 }
