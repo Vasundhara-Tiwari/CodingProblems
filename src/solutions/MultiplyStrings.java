d58ba58b4f7dc2165a352b71f67e86be809ef7a6
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-07-31 23:25:10 +0530

    adding solution of https://leetcode.com/problems/multiply-strings/

--- src/solutions/MultiplyStrings.java
+++ src/solutions/MultiplyStrings.java
@@ -2,6 +2,9 @@ import java.math.BigInteger;
 
 public class MultiplyStrings{
   public String multiply(String num1, String num2) {
-        
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        String result = n1.multiply(n2).toString();
+        return result;
     }
 }
