89be016181dbeb49cd20ed9aa26ca60ed2ee201a
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-26 22:07:37 +0530

    https://leetcode.com/problems/running-sum-of-1d-array/description/

--- src/solutions/RunningSum1DArray.java
+++ src/solutions/RunningSum1DArray.java
@@ -1,3 +1,13 @@
+import java.util.Arrays;
 public class RunningSum1DArray{
-
+    public int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result, 0);
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            result[i] = sum;
+        }
+        return result;
+    }
 }
