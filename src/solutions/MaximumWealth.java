355f7b32bc44e7aa199045d05e242e11729377a3
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-26 22:13:18 +0530

    Update MaximumWealth.java

--- src/solutions/MaximumWealth.java
+++ src/solutions/MaximumWealth.java
@@ -1,3 +1,13 @@
 public class MaximumWealth{
-  
+  public int maximumWealth(int[][] accounts) {
+    int maxSum = 0;
+    for(int i = 0; i < accounts.length; i++){
+      int sum = 0;
+      for(int j = 0; j < accounts[i].length; j++){
+        sum += accounts[i][j];
+      }
+      maxSum = sum > maxSum ? sum : maxSum;
+    }
+    return maxSum;
+  }
 }
