9e5686a832730ff30e119e463e1bee593ec6474c
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-26 22:20:26 +0530

    https://leetcode.com/problems/fizz-buzz/

--- src/solutions/FizzBuzz.java
+++ src/solutions/FizzBuzz.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
 public class FizzBuzz{
-
+  public List<String> fizzBuzz(int n) {
+        ArrayList<String> result = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                result.add("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                result.add("Fizz");
+            }
+            else if(i % 5 == 0){
+                result.add("Buzz");
+            }
+            else{
+                result.add(""+i);
+            }
+        }
+        return result;
+    }
 }
