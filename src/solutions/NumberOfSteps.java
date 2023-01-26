6d406911cc888c693fa8cb3575fffc742d6da88f
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-26 22:25:36 +0530

    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/

--- src/solutions/NumberOfSteps.java
+++ src/solutions/NumberOfSteps.java
@@ -1,3 +1,15 @@
 public class NumberOfSteps{
-
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        while(num > 0){
+            steps++;
+            if(num % 2 == 0){
+                num /= 2;
+            }
+            else{
+                num -= 1;
+            }
+        }
+        return steps;
+    }
 }
