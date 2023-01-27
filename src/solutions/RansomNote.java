b00536af68e68a97431b358be6fdcef7b2a85178
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-27 22:28:55 +0530

    https://leetcode.com/problems/ransom-note/description/

--- src/solutions/RansomNote.java
+++ src/solutions/RansomNote.java
@@ -1,3 +1,13 @@
 public class RansomNote{
-
+    public boolean canConstruct(String ransomNote, String magazine) {
+        for(int i = 0; i < ransomNote.length(); i++){
+            char ch = ransomNote.charAt(i);
+            int index = magazine.indexOf(ch);
+            if(index == -1){
+                return false;
+            }
+            magazine = magazine.substring(0, index) + magazine.substring(index+1);
+        }
+        return true;
+    }
 }
