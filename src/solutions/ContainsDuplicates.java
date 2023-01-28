971a266e0d7d8a8502eca4f1ab4d2b935b283b91
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-28 22:51:22 +0530

    https://leetcode.com/problems/contains-duplicate/description/

--- src/solutions/ContainsDuplicates.java
+++ src/solutions/ContainsDuplicates.java
@@ -1,3 +1,15 @@
+import java.util.HashSet;
 public class ContainsDuplicates{
-
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> al = new HashSet<>();
+        for(int i: nums){
+            if(!al.contains(i)){
+                al.add(i);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
 }
