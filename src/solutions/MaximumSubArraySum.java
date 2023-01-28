c51c5589b5485dc7d579586e3e17eb39d2589538
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-28 22:49:46 +0530

    https://leetcode.com/problems/maximum-subarray/description/

--- src/solutions/MaximumSubArraySum.java
+++ src/solutions/MaximumSubArraySum.java
@@ -1,3 +1,12 @@
 public class MaximumSubArraySum{
-
+    static int sum = 0;
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int result = nums[0];
+        for(int i=1; i < nums.length; i++){
+            max = Math.max(nums[i], max+nums[i]);
+            result = Math.max(result, max);
+        }
+        return result;
+    }
 }
