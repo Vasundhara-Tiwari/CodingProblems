c07dcf95a7b967974ef18323d709ac5dd7c4c823
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-30 01:48:37 +0530

    new file created

--- src/solutions/BestTimeToBuyAndSellStock.java
+++ src/solutions/BestTimeToBuyAndSellStock.java
@@ -1,3 +1,12 @@
 public class BestTimeToBuyAndSellStock{
-    
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0, max_profit = 0;
+        for(int i=0 ; i<prices.length ; i++){
+            min = Math.min(prices[i],min);
+            profit = prices[i] - min;
+            max_profit = Math.max(profit , max_profit);
+        }
+        return max_profit;
+    }
 }
