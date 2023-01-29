5529c96053888d47458dbd7ed7077112dd185f5d
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-29 22:28:49 +0530

    Update SearchIn2DMatrix.java

--- src/solutions/SearchIn2DMatrix.java
+++ src/solutions/SearchIn2DMatrix.java
@@ -1,3 +1,15 @@
 public class SearchIn2DMatrix{
-
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix[0].length;
+        for(int i = 0; i < matrix.length; i++){
+            if(matrix[i][0] <= target || matrix[i][n-1] >= target){
+                for(int j = 0; j < matrix[i].length; j++){
+                    if(matrix[i][j] == target){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
