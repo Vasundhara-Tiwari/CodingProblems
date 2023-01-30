e1cb83f24b668cbc27f75a8be6492508b94556f2
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-01-31 01:51:44 +0530

    https://leetcode.com/problems/intersection-of-two-arrays-ii/description/

--- src/solutions/IntersectionOfTwoArrays.java
+++ src/solutions/IntersectionOfTwoArrays.java
@@ -1,3 +1,20 @@
 public class IntersectionOfTwoArrays{
-
+    public int[] intersect(int[] nums1, int[] nums2) {
+        ArrayList<Integer>l=new ArrayList<>();
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    l.add(nums2[j]);
+                    nums2[j]=Integer.MAX_VALUE;
+                    break;
+                }
+            }
+        }
+        int k=0;
+        int arr[]= new int [l.size()];   
+        for(int m: l){ 
+            arr[k++]=m;
+        }
+        return arr;
+    }
 }
