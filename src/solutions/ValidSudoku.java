40506e777ec75a9abff152ee5ed7e4b66b29f4e2
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-02-01 22:55:55 +0530

    https://leetcode.com/problems/valid-sudoku/description/

--- src/solutions/ValidSudoku.java
+++ src/solutions/ValidSudoku.java
@@ -1,3 +1,18 @@
 public class ValidSudoku{
-
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> visited = new HashSet<>();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == '.') {
+                    continue;
+                }
+                
+                if(!visited.add(String.valueOf(board[i][j]) + " in row " + i) || !visited.add(String.valueOf(board[i][j]) + " in col " + j) || !visited.add(String.valueOf(board[i][j]) + " in box " + i/3 + "-" + j/3)) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
 }
