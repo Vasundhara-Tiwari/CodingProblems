35810e4634b4e62ba67fb7db6025870d1923073d
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-02-03 22:47:44 +0530

    https://leetcode.com/problems/remove-linked-list-elements/

--- src/solutions/RemoveLinkedListElements.java
+++ src/solutions/RemoveLinkedListElements.java
@@ -1,2 +1,23 @@
 public class RemoveLinkedListElements{
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null){
+            return head;
+        }
+        ListNode current = head.next;
+        ListNode prev = head;
+        while(current != null){
+            if(current.val == val){
+                prev.next = current.next;
+                current = current.next;
+            }
+            else{
+                current = current.next;
+                prev = prev.next;
+            }
+        }
+        if(head.val == val){
+            head = head.next;
+        }
+        return head;
+    }
 }
