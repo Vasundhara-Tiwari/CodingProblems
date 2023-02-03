a75ead29f8322cc59e51f5e9d2e846e8524d3eb8
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-02-03 22:46:01 +0530

    https://leetcode.com/problems/linked-list-cycle/

--- src/solutions/LinkedListCycle.java
+++ src/solutions/LinkedListCycle.java
@@ -1,2 +1,14 @@
 public class LinkedListCycle{
+    public boolean hasCycle(ListNode head) {
+        ArrayList<ListNode> al = new ArrayList<>();
+        ListNode current = head;
+        while(current != null){
+            if(al.contains(current.next)){
+                return true;
+            }
+            al.add(current.next);
+            current = current.next;
+        }
+        return false;
+    }
 }
