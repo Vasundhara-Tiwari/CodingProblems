ccf774c0587aa0263526a5916fe1e2319fc8b0e4
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-02-08 22:33:28 +0530

    https://leetcode.com/problems/invert-binary-tree/

--- src/solutions/InvertBinaryTree.java
+++ src/solutions/InvertBinaryTree.java
@@ -1,5 +1,14 @@
 package solutions;
 
 public class InvertBinaryTree{
-
+   public TreeNode invertTree(TreeNode root) {
+     if(root==null){
+        return null;
+     }
+     TreeNode left=invertTree(root.left);
+     TreeNode right=invertTree(root.right);
+     root.left=right;
+     root.right=left;
+     return root;
+   }
 }
