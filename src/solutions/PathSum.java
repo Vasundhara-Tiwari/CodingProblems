8f558c53aaa7765f86847f6b872acbe885aa415e
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-02-08 22:34:25 +0530

    https://leetcode.com/problems/path-sum/description/?envType=study-plan&id=data-structure-i

--- src/solutions/PathSum.java
+++ src/solutions/PathSum.java
@@ -1,5 +1,9 @@
 package solutions;
 
 public class PathSum{
-
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null) return false;
+        if(root.left==null && root.right==null && root.val==targetSum) return true;
+        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
+    }
 }
