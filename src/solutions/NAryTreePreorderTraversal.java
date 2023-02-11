e45d28fc07df2ffc0b00b746c8d1273b7e2cdd8c
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-02-11 22:49:42 +0530

    Update NAryTreePreorderTraversal.java

--- src/solutions/NAryTreePreorderTraversal.java
+++ src/solutions/NAryTreePreorderTraversal.java
@@ -1,3 +1,13 @@
 public class NAryTreePreorderTraversal{
-
+    List<Integer> result = new ArrayList<>();
+    
+    public List<Integer> preorder(Node root) {
+        if (root == null) return result;
+        result.add(root.val);
+        
+        for (Node child : root.children) {
+            preorder(child);
+        }
+        return result;
+    }
 }
